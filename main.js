0fb1ec0de711548b1fba5d9f258aa1f2cb80a459
Stanley Mair <stanleygmair@gmail.com>
2022-06-02 05:07:21 +1200

    Added my WIP to main.js

---
+++ main.js
@@ -0,0 +1,77 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G']
+    return dnaBases[Math.floor(Math.random() * 4)] 
+  }
+  
+  // Returns a random single strand of DNA containing 15 bases
+  const mockUpStrand = () => {
+    const newStrand = []
+    for (let i = 0; i < 15; i++) {
+      newStrand.push(returnRandBase())
+    }
+    return newStrand
+  }
+  
+  const pAequorFactory = (number, arr) => {
+    const dnaBases = ['A', 'T', 'C', 'G'];
+    return {
+      specimenNum: number,
+      dna: arr,
+      mutate() {
+        const randomIndex = Math.floor(Math.random() * 15);
+        const uniqueDnaBases = dnaBases.filter(base => base !== this.dna[randomIndex])
+        const randomBaseIndex = Math.floor(Math.random() * 3);
+        this.dna[randomIndex] = uniqueDnaBases[randomBaseIndex];
+        return this.dna;
+      },
+      compareDNA(obj) {
+        let dnaMatch = 0;
+        let percentageMatch;
+        for (let i = 0; i < 15; i++) {
+          if (this.dna[i] === obj.dna[i]) {
+            dnaMatch++;
+          }
+        }
+        percentageMatch = dnaMatch / 15 * 100;
+        return `specimen #${this.specimenNum} and specimen #${obj.specimenNum} have ${percentageMatch}% DNA in common`;
+      },
+      willLikelySurvive() {
+        let survivalProbability = 15 * 0.6;
+        let dnaBaseIsCG = 0;
+        for (let i = 0; i < 15; i++) {
+          if (this.dna[i] === 'C' || this.dna[i] === 'G')
+          dnaBaseIsCG++
+        }
+        if (dnaBaseIsCG < survivalProbability) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+  }
+  
+  const createThirtyPAequorInstances = () => {
+    const thirtyPAequorInstances = [];
+    let i = 0;
+    let j = 0;
+    do {
+      thirtyPAequorInstances[i] = pAequorFactory(i, mockUpStrand());
+      if (thirtyPAequorInstances[i].willLikelySurvive() === false) {
+        thirtyPAequorInstances.pop();
+        i = i;
+      } else {
+        i++;
+      }
+    } while (i < 30);
+    return thirtyPAequorInstances;
+  }
+  
+  console.log(createThirtyPAequorInstances());
+  
+  
+  
+  
+  
+  
